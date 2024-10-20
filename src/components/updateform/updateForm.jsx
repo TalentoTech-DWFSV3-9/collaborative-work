08f765a494ec14b7e950440bc704367eaa73256a
gcorrea2005 <gcorrea2005@gmail.com>
2024-10-20 16:11:09 -0500

    Ingreso UpdateForm -- prop/types

--- src/components/updateform/updateForm.jsx
+++ src/components/updateform/updateForm.jsx
@@ -1,9 +1,10 @@
 // src/components/UpdateForm.jsx
-import { useState } from 'react';
+import { useState } from "react";
+import propTypes from "prop-types";
 
 const UpdateForm = ({ updateUser }) => {
-  const [newName, setNewName] = useState('');
-  const [newEmail, setNewEmail] = useState('');
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,22 +15,16 @@ const UpdateForm = ({ updateUser }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h3>Actualizar Información</h3>
-      <input
-        type="text"
-        placeholder="Nuevo nombre"
-        value={newName}
-        onChange={(e) => setNewName(e.target.value)}
-      />
-      <input
-        type="email"
-        placeholder="Nuevo correo"
-        value={newEmail}
-        onChange={(e) => setNewEmail(e.target.value)}
-      />
+      <h3>Actualizar Información de Usuario</h3>
+      <input type="text" placeholder="Nuevo nombre" value={newName} onChange={(e) => setNewName(e.target.value)} />
+      <input type="email" placeholder="Nuevo correo" value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
       <button type="submit">Actualizar</button>
     </form>
   );
+};
+
+UpdateForm.propTypes = {
+  updateUser: propTypes.func.isRequired,
 };
 
 export default UpdateForm;
